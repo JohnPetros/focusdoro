d9680ec93af4fef15da594f349e295c501fe6862
João Pedro <joaopedro.nc@outlook.com>
2023-10-03 20:01:52 -0300

    🔧 chore: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@types': './src/@types',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@contexts': './src/contexts',
+            '@storage': './src/storage',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+      'transform-inline-environment-variables',
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
