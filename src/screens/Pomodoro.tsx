fb6cd1aa8fd2a89b2c1e9e0e1512fa9d67f5c773
João Pedro <joaopedro.nc@outlook.com>
2023-10-04 20:18:07 -0300

    ✨ feat (ui): add TaskCard and RoundButton to Pomodoro screen

--- src/screens/Pomodoro.tsx
+++ src/screens/Pomodoro.tsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from 'react'
 import { Dimensions, StyleSheet } from 'react-native'
+
 import Animated, {
   useAnimatedProps,
   useDerivedValue,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated'
-import { ReText } from 'react-native-redash'
+
 import { Svg, Circle } from 'react-native-svg'
-import { Stack, Text, View, YStack, useTheme } from 'tamagui'
-import { calculateRadius } from '../utils/functions/calculateRadius'
+import { ReText } from 'react-native-redash'
+import { Square, Text, View, YStack, useTheme } from 'tamagui'
 import { XStack } from 'tamagui'
+import { RoundButton } from '../components/RoundButton'
+import { TaskCard } from '../components/TaskCard'
+
+import { Pause } from 'phosphor-react-native'
+
+import { calculateRadius } from '../utils/functions/calculateRadius'
 
 const { width, height } = Dimensions.get('window')
 
@@ -22,12 +29,12 @@ const RADIUS = calculateRadius(CIRCLE_LENGTH)
 const OUTER_CIRCLE_RADIUS = calculateRadius(OUTER_CIRCLE_LENGTH)
 const INNER_CIRCLE_RADIUS = calculateRadius(INNER_CIRCLE_LENGTH)
 
-const ORIGINAL_TOTAL_SECONDS = 60
+const SESSION_TOTAL_SECONDS = 900
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
 export function Pomodoro() {
-  const [totalSeconds, setTotalSeconds] = useState(ORIGINAL_TOTAL_SECONDS)
+  const [totalSeconds, setTotalSeconds] = useState(SESSION_TOTAL_SECONDS)
   const theme = useTheme()
 
   const progress = useSharedValue(1)
@@ -71,58 +78,68 @@ export function Pomodoro() {
 
       setTotalSeconds(totalSeconds - 1)
 
-      progress.value = withTiming(totalSeconds / ORIGINAL_TOTAL_SECONDS)
+      progress.value = withTiming(totalSeconds / SESSION_TOTAL_SECONDS)
     }, 1000)
   }, [totalSeconds])
 
   return (
-    <YStack f={1} ai="center" jc="center" position="relative">
-      <XStack ai="center" gap={4} zIndex={50}>
-        <ReText style={reTextStyle.style} text={animatedMinutesText} />
-        {/* <Text color="$blue11" fontSize={48}>
-          {minutes.toString().padStart(2, '0')}
-        </Text> */}
-        <Text color="$blue11" fontSize={48}>
-          :
-        </Text>
-        <ReText style={reTextStyle.style} text={animatedSecondsText} />
+    <YStack f={1} pt={40}>
+      <Square pt={40} px={24} w="100%" position="absolute" ai="center">
+        <TaskCard />
+      </Square>
+      <YStack f={1} ai="center" jc="center" position="relative">
+        <XStack ai="center" gap={4} zIndex={50}>
+          <ReText style={reTextStyle.style} text={animatedMinutesText} />
+          <Text color="$blue11" fontSize={48}>
+            :
+          </Text>
+          <ReText style={reTextStyle.style} text={animatedSecondsText} />
 
-        {/* <Text color="$blue11" fontSize={48}>
+          {/* <Text color="$blue11" fontSize={48}>
           {seconds.toString().padStart(2, '0')}
         </Text> */}
-      </XStack>
-      <Svg style={{ position: 'absolute' }}>
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={RADIUS}
-          stroke={theme.blue4.val}
-          strokeWidth={32}
-        />
-        <AnimatedCircle
-          cx={width / 2}
-          cy={height / 2}
-          r={OUTER_CIRCLE_RADIUS}
-          stroke={theme.blue6.val}
-          fill={theme.blue2.val}
-          strokeWidth={24}
-          strokeDasharray={OUTER_CIRCLE_LENGTH}
-          animatedProps={outerCircleAnimatedProps}
-        />
-        <View position="relative">
-          {/* <View w={24} h={24}  top={0} left={0} position="absolute" zIndex={150} bg="$red11" /> */}
+        </XStack>
+        <Svg style={{ position: 'absolute' }}>
+          <Circle
+            cx={width / 2}
+            cy={height / 2}
+            r={RADIUS}
+            stroke={theme.blue4.val}
+            strokeWidth={32}
+          />
           <AnimatedCircle
             cx={width / 2}
             cy={height / 2}
-            r={INNER_CIRCLE_RADIUS}
-            stroke={theme.blue11.val}
+            r={OUTER_CIRCLE_RADIUS}
+            stroke={theme.blue6.val}
             fill={theme.blue2.val}
-            strokeWidth={8}
-            strokeDasharray={INNER_CIRCLE_LENGTH}
-            animatedProps={innerCircleAnimatedProps}
+            strokeWidth={24}
+            strokeDasharray={OUTER_CIRCLE_LENGTH}
+            animatedProps={outerCircleAnimatedProps}
           />
-        </View>
-      </Svg>
+          <View position="relative">
+            {/* <View w={24} h={24}  top={0} left={0} position="absolute" zIndex={150} bg="$red11" /> */}
+            <AnimatedCircle
+              cx={width / 2}
+              cy={height / 2}
+              r={INNER_CIRCLE_RADIUS}
+              stroke={theme.blue11.val}
+              fill={theme.blue2.val}
+              strokeWidth={8}
+              strokeDasharray={INNER_CIRCLE_LENGTH}
+              animatedProps={innerCircleAnimatedProps}
+            />
+          </View>
+        </Svg>
+
+        <YStack position="absolute" bottom={124}>
+          <RoundButton
+            size="$6"
+            icon={<Pause color={theme.blue12.val} size={32} />}
+            bc="$blue10"
+          />
+        </YStack>
+      </YStack>
     </YStack>
   )
 }
