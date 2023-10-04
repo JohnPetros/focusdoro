48f92bbe0a9bb803169f956b2e4b4a11ab49a3c7
João Pedro <joaopedro.nc@outlook.com>
2023-10-04 17:21:25 -0300

    ✨ feat (ui): add pomodoro base  timer

--- src/screens/Pomodoro.tsx
+++ src/screens/Pomodoro.tsx
@@ -1,19 +1,94 @@
-import { Dimensions } from "react-native"
-import { Svg, Circle } from "react-native-svg"
-import { YStack, useTheme } from "tamagui"
+import { useEffect } from 'react'
+import { Dimensions, StyleSheet } from 'react-native'
+import Animated, {
+  useAnimatedProps,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
+import { ReText } from 'react-native-redash'
+import { Svg, Circle } from 'react-native-svg'
+import { Stack, View, YStack, useTheme } from 'tamagui'
+import { calculateRadius } from '../utils/functions/calculateRadius'
 
 const { width, height } = Dimensions.get('window')
 
 const CIRCLE_LENGTH = 1000
-const RADIUS = CIRCLE_LENGTH / (2 * Math.PI)
+const OUTER_CIRCLE_LENGTH = 1000
+const INNER_CIRCLE_LENGTH = 900
+
+const RADIUS = calculateRadius(CIRCLE_LENGTH)
+const OUTER_CIRCLE_RADIUS = calculateRadius(OUTER_CIRCLE_LENGTH)
+const INNER_CIRCLE_RADIUS = calculateRadius(INNER_CIRCLE_LENGTH)
+
+const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
 export function Pomodoro() {
   const theme = useTheme()
 
+  const progress = useSharedValue(0)
+
+  const innerCircleAnimatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: INNER_CIRCLE_LENGTH * (1 - progress.value),
+  }))
+
+  const outerCircleAnimatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: OUTER_CIRCLE_LENGTH * (1 - progress.value),
+  }))
+
+  const animatedText = useDerivedValue(() => {
+    return `${Math.floor(progress.value * 100)}`
+  })
+
+  const reTextStyle = StyleSheet.create({
+    style: {
+      fontSize: 64,
+      color: theme.blue11.val,
+      textAlign: 'center',
+    },
+  })
+
+  useEffect(() => {
+    progress.value = withTiming(1, { duration: 2000 })
+  }, [])
+
   return (
-    <YStack f={1} bg="$blue1">
-      <Svg>
-        <Circle cx={width / 2} cy={height / 2} r={RADIUS} stroke={theme.blue3.toString()} strokeWidth={20} />
+    <YStack f={1} ai="center" jc="center" position="relative">
+      <YStack zIndex={50}>
+        <ReText style={reTextStyle.style} text={animatedText} />
+      </YStack>
+      <Svg style={{ position: 'absolute' }}>
+        <Circle
+          cx={width / 2}
+          cy={height / 2}
+          r={RADIUS}
+          stroke={theme.blue4.val}
+          strokeWidth={32}
+        />
+        <AnimatedCircle
+          cx={width / 2}
+          cy={height / 2}
+          r={OUTER_CIRCLE_RADIUS}
+          stroke={theme.blue6.val}
+          fill={theme.blue2.val}
+          strokeWidth={24}
+          strokeDasharray={OUTER_CIRCLE_LENGTH}
+          animatedProps={outerCircleAnimatedProps}
+        />
+        <View position='relative'>
+          <View w={24} h={24}  top={0} left={0} position="absolute" zIndex={150} bg="$red11" />
+          <AnimatedCircle
+            cx={width / 2}
+            cy={height / 2}
+            r={INNER_CIRCLE_RADIUS}
+            stroke={theme.blue11.val}
+            fill={theme.blue2.val}
+            strokeWidth={8}
+            strokeDasharray={INNER_CIRCLE_LENGTH}
+            strokeLinecap="round"
+            animatedProps={innerCircleAnimatedProps}
+          />
+        </View>
       </Svg>
     </YStack>
   )
