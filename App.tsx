95fa2508eb685412922cddeeece7c150012fee30
João Pedro <joaopedro.nc@outlook.com>
2023-10-04 20:18:24 -0300

    ✨ feat (ui): load expo fonts

--- App.tsx
+++ App.tsx
@@ -1,32 +1,31 @@
-import { useFonts } from 'expo-font';
-import { TamaguiProvider, YStack, Text } from 'tamagui';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { TamaguiProvider, YStack, Theme } from 'tamagui';
 import { StatusBar } from 'react-native';
-
 import config from './tamagui.config';
+
 import { Pomodoro } from './src/screens/Pomodoro';
 
 export default function App() {
-  const [isFontloading] = useFonts({
-    regular: require('@tamagui/font-inter/otf/Inter-Refular.otf'),
-    medium: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
-    semibold: require('@tamagui/font-inter/otf/Inter-Semibold.otf'),
-    bold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
-  });
+  const [fontsLoaded, fontError] = useFonts({
+    Inter_900Black,
+  })
 
-  if (!isFontloading) {
+  if (!fontsLoaded && !fontError) {
     return null;
   }
 
   return (
     <TamaguiProvider config={config}>
+    <Theme name='dark'>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
-      <YStack f={1} bg="$blue1">
+      <YStack f={1} bg='$blue2'>
         <Pomodoro />
       </YStack>
-    </TamaguiProvider>
+    </Theme>
+  </TamaguiProvider>
   );
 }
