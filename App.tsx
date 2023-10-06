38691fefbd998441a5a8b625c96351bac4ad4622
João Pedro <joaopedro.nc@outlook.com>
2023-10-06 10:30:03 -0300

    ✨ feat (ui): add safe area view to app

--- App.tsx
+++ App.tsx
@@ -1,31 +1,46 @@
-import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
-import { TamaguiProvider, YStack, Theme } from 'tamagui';
-import { StatusBar } from 'react-native';
-import config from './tamagui.config';
+import { useEffect } from 'react'
+import { useFonts } from '@expo-google-fonts/inter'
+import { StatusBar } from 'react-native'
+import { TamaguiProvider, YStack, Theme } from 'tamagui'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import config from './tamagui.config'
 
-import { Pomodoro } from './src/screens/Pomodoro';
+import { ToastProvider, ToastViewport } from '@tamagui/toast'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import { Pomodoro } from './src/screens/Pomodoro'
+import { Toast } from './src/components/Toast'
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter_900Black,
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   })
 
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
+  if (!loaded) return null
+
+  // useEffect(() => {
+  //   SplashScreen.hideAsync()
+  // }, [])
 
   return (
     <TamaguiProvider config={config}>
-    <Theme name='dark'>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <YStack f={1} bg='$blue2'>
-        <Pomodoro />
-      </YStack>
-    </Theme>
-  </TamaguiProvider>
-  );
+      <ToastProvider native={false} swipeDirection="down" duration={100000}>
+        <SafeAreaProvider>
+          <Toast />
+          <ToastViewport left="50%" bottom={40} />
+          <Theme name="dark">
+            <StatusBar
+              barStyle="light-content"
+              backgroundColor="transparent"
+              translucent
+            />
+            <YStack f={1} bg="$blue2">
+              <Pomodoro />
+            </YStack>
+          </Theme>
+        </SafeAreaProvider>
+      </ToastProvider>
+    </TamaguiProvider>
+  )
 }
