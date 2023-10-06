d2906bcc408189275dbd4904ccb893eca49496f2
João Pedro <joaopedro.nc@outlook.com>
2023-10-06 16:03:16 -0300

    ✨ feat: go to settings screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,31 @@
 import { useState } from 'react'
-import { Pressable } from 'react-native'
-import { Link, useRouter } from 'expo-router'
+import { useRouter } from 'expo-router'
+import { useToastController } from '@tamagui/toast'
 
 import { H2, Input, XStack, YStack, useTheme } from 'tamagui'
 import { TaskCard } from '../components/TaskCard'
 import { RoundButton } from '../components/RoundButton'
-import { Plus } from 'phosphor-react-native'
+import { Play, Plus, XCircle } from 'phosphor-react-native'
 
 export default function Home() {
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const theme = useTheme()
+  const router = useRouter()
+  const toast = useToastController()
+
+  function handleNewTaskButton() {
+    if (newTaskTitle.length < 3) {
+      toast.show('Add a valid task title', {
+        icon: XCircle,
+      })
+      return
+    }
+
+    router.push('/settings')
+  }
 
   return (
-    <YStack f={1} bc="$blue2" pt={40} px={24}>
+    <YStack f={1} bc="$blue2" pt={44} px={24}>
       <XStack ai="center" jc="space-between" gap={8}>
         <Input
           bg="$colorTransparent"
@@ -23,6 +36,7 @@ export default function Home() {
           w="85%"
           h={56}
           p={12}
+          fontSize={16}
           placeholder="What are you workin on?"
           placeholderTextColor="$blue12"
           value={newTaskTitle}
@@ -33,21 +47,16 @@ export default function Home() {
           size="$3"
           bg="$blue10"
           icon={<Plus color={theme.blue12.val} size={24} weight="bold" />}
+          onPress={handleNewTaskButton}
         />
       </XStack>
-      <H2 fontSize={24} color="$blue12" mt={24}>
+      <H2 fontSize={16} color="$blue12" mt={24} letterSpacing={1.1}>
         Tasks (3)
       </H2>
       <YStack gap={12}>
-        <Pressable>
-          <TaskCard totalSessions={3} completedSessions={1} />
-        </Pressable>
-        <Pressable>
-          <TaskCard totalSessions={3} completedSessions={1} />
-        </Pressable>
-        <Pressable>
-          <TaskCard totalSessions={3} completedSessions={1} />
-        </Pressable>
+        <TaskCard totalSessions={3} completedSessions={1} icon={Play} />
+        <TaskCard totalSessions={3} completedSessions={1} icon={Play} />
+        <TaskCard totalSessions={3} completedSessions={1} icon={Play} />
       </YStack>
     </YStack>
   )
