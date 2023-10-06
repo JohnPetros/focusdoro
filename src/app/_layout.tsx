9719367b04220c5a5b799265a7678684ac4144bd
João Pedro <joaopedro.nc@outlook.com>
2023-10-06 16:31:56 -0300

    ✨ feat: pick number from number picker

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,7 @@ import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from '@expo-google-fonts/inter'
 import { TamaguiProvider, YStack, Theme } from 'tamagui'
 import config from '../../tamagui.config'
-
+import { PortalProvider } from '@gorhom/portal';
 import { ToastProvider, ToastViewport } from '@tamagui/toast'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Toast } from '../components/Toast'
@@ -24,24 +24,26 @@ export default function App() {
   return (
     <TamaguiProvider config={config}>
       <ToastProvider native={false} swipeDirection="down" duration={5000}>
-        <SafeAreaProvider>
-          <Toast />
-          <ToastViewport left="50%" bottom={40} />
-          <Theme name="dark">
-            <StatusBar
-              barStyle="light-content"
-              backgroundColor="transparent"
-              translucent
-            />
-            <YStack f={1} bg="$blue2">
-              <Stack
-                screenOptions={{
-                  headerShown: false,
-                }}
+        <PortalProvider>
+          <SafeAreaProvider>
+            <Toast />
+            <ToastViewport left="50%" bottom={40} />
+            <Theme name="dark">
+              <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
               />
-            </YStack>
-          </Theme>
-        </SafeAreaProvider>
+              <YStack f={1} bg="$blue2">
+                <Stack
+                  screenOptions={{
+                    headerShown: false,
+                  }}
+                />
+              </YStack>
+            </Theme>
+          </SafeAreaProvider>
+        </PortalProvider>
       </ToastProvider>
     </TamaguiProvider>
   )
