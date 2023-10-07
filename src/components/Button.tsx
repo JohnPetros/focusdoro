781ad6ed508bf8cc2211a465553dc6406fc5aa33
João Pedro <joaopedro.nc@outlook.com>
2023-10-07 16:37:55 -0300

    ✨ feat (ui): add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import { Button as TamaguiButton, styled } from 'tamagui'
+
+export const Button = styled(TamaguiButton, {
+  width: '100%',
+  height: '$5',
+  color: '$blue12',
+  fontSize: 16,
+  borderRadius: 24,
+
+  variants: {
+    background: {
+      normal: {
+        bg: '$blue8',
+      },
+      outline: {
+        borderWidth: 2,
+        borderColor: '$blue10',
+      },
+    },
+  } as const,
+
+  defaultVariants: {
+    background: 'normal',
+  },
+})
