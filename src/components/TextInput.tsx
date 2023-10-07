bdd659aab4abb37ba8b33697acb4900371a7a32f
João Pedro <joaopedro.nc@outlook.com>
2023-10-07 16:41:30 -0300

    ✨ feat (ui): add TextInput component

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,20 @@
+import { Input, InputProps } from 'tamagui'
+
+interface TextInputProps extends InputProps {}
+
+export function TextInput({ ...rest }: TextInputProps) {
+  return (
+    <Input
+      {...rest}
+      bg="$colorTransparent"
+      borderWidth={2}
+      borderColor="$blue6"
+      focusStyle={{ borderColor: '$blue10', color: '$blue12' }}
+      h={56}
+      p={12}
+      fontSize={16}
+      color="$blue12"
+      placeholderTextColor="$blue12"
+    />
+  )
+}
