245df8c2648856aeb0a3b729408a36a56ae9e3a3
João Pedro <joaopedro.nc@outlook.com>
2023-10-07 20:57:15 -0300

    🐛 fix: return minutesValue instead of seconds

--- src/utils/functions/convertSecondsToTime.ts
+++ src/utils/functions/convertSecondsToTime.ts
@@ -2,7 +2,7 @@ export function convertSecondsToTime(seconds: number) {
   const minutesValue = Math.floor(seconds / 60)
 
   if (minutesValue > 1) {
-    return `${seconds} minutes`
+    return `${minutesValue} minutes`
   }
 
   return `${seconds} seconds`
