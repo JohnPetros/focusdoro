44cbc421fd07bd358691f132b45053a175364ed4
João Pedro <joaopedro.nc@outlook.com>
2023-10-09 20:28:40 -0300

    🏷️ type: make shadowColor prop optional

--- src/components/RoundButton.tsx
+++ src/components/RoundButton.tsx
@@ -2,14 +2,14 @@ import { Button, useTheme, ButtonProps } from 'tamagui'
 import { Shadow } from 'react-native-shadow-2'
 
 interface RoundButtonProps extends ButtonProps {
-  shadowColor: string;
+  shadowColor: string
 }
 
 export function RoundButton({ color, shadowColor, ...rest }: RoundButtonProps) {
   const theme = useTheme()
 
   return (
-    <Shadow startColor={shadowColor} style={{ borderRadius: 40 }}>
+    <Shadow startColor={shadowColor ?? ''} style={{ borderRadius: 40 }}>
       <Button
         {...rest}
         circular
