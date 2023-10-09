cd5e5199299fd92652bbe5e3ba89c120bac4285c
João Pedro <joaopedro.nc@outlook.com>
2023-10-09 20:37:03 -0300

    ✨ feat: hide splash screen on fonts loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,26 @@
-import { useEffect } from 'react'
 import { StatusBar } from 'react-native'
-import { SplashScreen, Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+
+import {  Stack } from 'expo-router'
 import { useFonts } from '@expo-google-fonts/inter'
 import { TamaguiProvider, YStack, Theme } from 'tamagui'
 import config from '../../tamagui.config'
-import { PortalProvider } from '@gorhom/portal';
+import { PortalProvider } from '@gorhom/portal'
 import { ToastProvider, ToastViewport } from '@tamagui/toast'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Toast } from '../components/Toast'
 
 export default function App() {
-  const [loaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   })
 
-  if (!loaded) return null
+  if (!fontsLoaded) return null
 
-  // useEffect(() => {
-  //   SplashScreen.hideAsync()
-  // }, [])
+  async function handleLayoutRootView() {
+    if (fontsLoaded) await SplashScreen.hideAsync()
+  }
 
   return (
     <TamaguiProvider config={config}>
@@ -34,7 +35,7 @@ export default function App() {
                 backgroundColor="transparent"
                 translucent
               />
-              <YStack f={1} bg="$blue2">
+              <YStack f={1} bg="$blue2" onLayout={handleLayoutRootView}>
                 <Stack
                   screenOptions={{
                     headerShown: false,
