2a66480f91ac13b14ffeb67f95e94f3796a7ddbd
João Pedro <joaopedro.nc@outlook.com>
2023-10-13 20:59:25 -0300

    🔧 chore: update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  root: true,
+  plugins: ["simple-import-sort"],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "prettier/prettier": ["warn", { usePrettierrc: true, endOfLine: "auto" }],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          ["^\\u0000"],
+          ["^react", "^@?\\w"],
+          ["^(@prism)(/.*|$)"],
+          ["^(@env)(/.*|$)"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
+  },
+}
