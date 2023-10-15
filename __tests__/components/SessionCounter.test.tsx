04ac8741aa5ca754c133c05360eada09f0a3a53e
João Pedro <joaopedro.nc@outlook.com>
2023-10-15 15:04:56 -0300

    🧪 test: SessionCounter component

---
+++ __tests__/components/SessionCounter.test.tsx
@@ -0,0 +1,42 @@
+import { render } from "@testing-library/react-native"
+import { Play } from "phosphor-react-native"
+import { TamaguiProvider } from "tamagui"
+
+import { SessionCounter } from "../../components/SessionCounter"
+import config from "../../tamagui.config"
+
+const wrapper = ({ children }) => (
+  <TamaguiProvider config={config}>{children}</TamaguiProvider>
+)
+
+describe("SessionCounter component", () => {
+  it("should render icons according to total sessions amount", () => {
+    const totalAmount = 3
+
+    const { getAllByTestId } = render(
+      <SessionCounter
+        isActive={true}
+        completedAmount={1}
+        totalAmount={totalAmount}
+      />,
+      { wrapper }
+    )
+
+    expect(getAllByTestId(/filled|not-filled/i)).toHaveLength(totalAmount)
+  })
+
+  it("should render filled icons according to completed sessions amount", () => {
+    const completedAmount = 2
+
+    const { getAllByTestId } = render(
+      <SessionCounter
+        isActive={true}
+        completedAmount={completedAmount}
+        totalAmount={4}
+      />,
+      { wrapper }
+    )
+
+    expect(getAllByTestId(/filled/i)).toHaveLength(completedAmount + 2)
+  })
+})
