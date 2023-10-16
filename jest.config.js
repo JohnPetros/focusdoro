78a3620a0424e486723c7587d2b81e36f35261a4
João Pedro <joaopedro.nc@outlook.com>
2023-10-16 00:44:20 -0300

    🔧 chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// jest.config.js
+/** @type {import('jest').Config} */
+
+const config = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  modulePathIgnorePatterns: ["/Mocks\\.ts$/"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+}
+
+module.exports = config
