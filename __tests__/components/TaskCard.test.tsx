b9948c6d3069d834c81331cc9e04f05890237ecd
João Pedro <joaopedro.nc@outlook.com>
2023-10-16 00:45:45 -0300

    🧪 test: add TaskCard component

--- __tests__/components/TaskCard.test.tsx
+++ __tests__/components/TaskCard.test.tsx
@@ -4,50 +4,65 @@ import { TamaguiProvider } from "tamagui"
 
 import { TaskCard } from "../../components/TaskCard"
 import config from "../../tamagui.config"
+import { tasksMock } from "../mocks/tasksMock"
 
 const wrapper = ({ children }) => (
   <TamaguiProvider config={config}>{children}</TamaguiProvider>
 )
 
-const task = {
-  title: "title mock",
-  icon: Play,
-  completedSessions: 1,
-  totalSessions: 3,
-  isActive: false,
-  onPress: () => console.log("mock"),
-}
+const taskMock = tasksMock[0]
+const onPressMock = jest.fn()
 
 describe("TaskCard component", () => {
   it("should render task title", () => {
     const { getByText } = render(
       <TaskCard
-        title={task.title}
-        icon={task.icon}
-        completedSessions={task.completedSessions}
-        totalSessions={task.totalSessions}
-        isActive={task.isActive}
-        onPress={task.onPress}
+        title={taskMock.title}
+        icon={Play}
+        completedSessions={taskMock.completedSessions}
+        totalSessions={taskMock.totalSessions}
+        isActive={true}
+        onPress={onPressMock}
       />,
       { wrapper }
     )
 
-    expect(getByText(task.title)).toBeTruthy()
+    expect(getByText(taskMock.title)).toBeTruthy()
   })
 
   it("should call a function on press", () => {
-    const functionMock = jest.fn()
-
     const taskActionLabel = "Run task action"
 
     const { getByLabelText } = render(
       <TaskCard
-        title={task.title}
-        icon={task.icon}
-        completedSessions={task.completedSessions}
-        totalSessions={task.totalSessions}
-        isActive={task.isActive}
-        onPress={functionMock}
+        title={taskMock.title}
+        icon={Play}
+        completedSessions={taskMock.completedSessions}
+        totalSessions={taskMock.totalSessions}
+        isActive={true}
+        onPress={onPressMock}
+      />,
+      { wrapper }
+    )
+
+    const taskButton = getByLabelText(taskActionLabel)
+
+    fireEvent.press(taskButton)
+
+    expect(onPressMock).toBeCalled()
+  })
+
+  it("should call a function on press", () => {
+    const taskActionLabel = "Run task action"
+
+    const { getByLabelText } = render(
+      <TaskCard
+        title={taskMock.title}
+        icon={Play}
+        completedSessions={taskMock.completedSessions}
+        totalSessions={taskMock.totalSessions}
+        isActive={true}
+        onPress={onPressMock}
         label={taskActionLabel}
       />,
       { wrapper }
@@ -57,31 +72,6 @@ describe("TaskCard component", () => {
 
     fireEvent.press(taskButton)
 
-    expect(functionMock).toBeCalled()
-  })
-
-  it("should call a function on press", () => {
-    const functionMock = jest.fn()
-
-    const taskActionLabel = "Run task action"
-
-    const { getByLabelText } = render(
-      <TaskCard
-        title={task.title}
-        icon={task.icon}
-        completedSessions={task.completedSessions}
-        totalSessions={task.totalSessions}
-        isActive={task.isActive}
-        onPress={functionMock}
-        label={taskActionLabel}
-      />,
-      { wrapper }
-    )
-
-    const taskButton = getByLabelText(taskActionLabel)
-
-    fireEvent.press(taskButton)
-
-    expect(functionMock).toBeCalled()
+    expect(onPressMock).toBeCalled()
   })
 })
