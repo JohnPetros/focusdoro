2a99c0f14cb8b62e5793b0397163888524998b7d
João Pedro <joaopedro.nc@outlook.com>
2023-10-16 21:01:21 -0300

    ✨ feat (utils): add audios

---
+++ utils/audios.ts
@@ -0,0 +1,45 @@
+import {
+  CloudRain,
+  Coffee,
+  Icon,
+  Peace,
+  Tree,
+  Waves,
+} from "phosphor-react-native"
+
+type Audio = {
+  title: string
+  file: string
+  icon: Icon
+}
+
+const BASE_URL =
+  "https://joeweaver.me/codepenassets/freecodecamp/challenges/build-a-pomodoro-clock"
+
+export const audios: Audio[] = [
+  {
+    title: "forest",
+    file: `${BASE_URL}/forest.mp3`,
+    icon: Tree,
+  },
+  {
+    title: "coffee",
+    file: `${BASE_URL}/coffee.mp3`,
+    icon: Coffee,
+  },
+  {
+    title: "ocean",
+    file: `${BASE_URL}/ocean.mp3`,
+    icon: Waves,
+  },
+  {
+    title: "rain",
+    file: `${BASE_URL}/rain.mp3`,
+    icon: CloudRain,
+  },
+  {
+    title: "peace",
+    file: `${BASE_URL}/peace.mp3`,
+    icon: Peace,
+  },
+]
