84d8b09ede54d7c1eb6ba0f7e25712d54d09bdbc
João Pedro <joaopedro.nc@outlook.com>
2023-10-18 19:19:05 -0300

    ✨ feat (services): update timer notification

--- services/notification/index.ts
+++ services/notification/index.ts
@@ -1,36 +1,7 @@
-import { useEffect, useState } from "react"
-import {
-  AndroidImportance,
-  AndroidVisibility,
-  AuthorizationStatus,
-} from "@notifee/react-native"
-import notifee from "@notifee/react-native"
-
-import { CHANNELS } from "./channels"
 import { timerNotification } from "./timerNotification"
 
 export function useNotification() {
-  const [timerChannelId, setTimerChannelId] = useState("")
-
-  async function init() {
-    if (AuthorizationStatus.DENIED) await notifee.requestPermission()
-
-    const timerChannelId = await notifee.createChannel({
-      id: CHANNELS.timer,
-      name: CHANNELS.timer,
-      vibration: false,
-      importance: AndroidImportance.HIGH,
-      visibility: AndroidVisibility.PUBLIC,
-    })
-
-    setTimerChannelId(timerChannelId)
-  }
-
-  useEffect(() => {
-    init()
-  }, [])
-
   return {
-    ...timerNotification(timerChannelId),
+    ...timerNotification(),
   }
 }
--- services/notification/timerNotification.ts
+++ services/notification/timerNotification.ts
@@ -1,30 +1,68 @@
+import { useRef } from "react"
 import uuid from "react-native-uuid"
-import notifee from "@notifee/react-native"
+import notifee, {
+  AndroidImportance,
+  AndroidVisibility,
+  AuthorizationStatus,
+} from "@notifee/react-native"
 
 import { ITimerNotification } from "./interfaces/ITimerNotification"
+import { CHANNELS } from "./channels"
 
-export function timerNotification(channelId: string): ITimerNotification {
+export function timerNotification(): ITimerNotification {
+  const timerChannelId = useRef("")
+  const timerId = useRef("")
+
   return {
-    async displayTimer({ title, taskId }) {
-      if (!channelId) return
+    async displayTimer(taskId) {
+      if (AuthorizationStatus.DENIED) await notifee.requestPermission()
+
+      const channelId = await notifee.createChannel({
+        id: CHANNELS.timer,
+        name: CHANNELS.timer,
+        vibration: false,
+        importance: AndroidImportance.HIGH,
+        visibility: AndroidVisibility.PUBLIC,
+      })
+
+      timerChannelId.current = channelId
 
       const id = uuid.v4().toString()
 
-      console.log({ channelId })
-
       await notifee.displayNotification({
         id,
-        title: `<strong>${title}</strong>`,
+        title: `<strong>Timer running</strong>`,
         data: {
           taskId,
         },
         android: {
           channelId,
           actions: [{ title: "pause", pressAction: { id: "pause-timer" } }],
+          onlyAlertOnce: true,
+          progress: {
+            max: 10,
+            current: 0,
+          },
         },
       })
 
-      return id
+      timerId.current = id
+    },
+
+    async updateTimer({ title, actions, progress }) {
+      if (timerId.current && timerChannelId.current)
+        await notifee.displayNotification({
+          id: timerId.current,
+          title,
+          android: {
+            channelId: timerChannelId.current,
+            progress: {
+              max: 10,
+              current: progress,
+            },
+            actions,
+          },
+        })
     },
   }
 }
