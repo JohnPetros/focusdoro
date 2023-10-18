2a0e130275c13a73ec04cae3ac4deed6eec7b487
João Pedro <joaopedro.nc@outlook.com>
2023-10-18 14:51:37 -0300

    ✨ feat (utils): add features default data

---
+++ utils/features.ts
@@ -0,0 +1,20 @@
+import { Feature } from "../@types/feature"
+
+export const features: Feature[] = [
+  {
+    title: "vibration",
+    isActive: true,
+  },
+  {
+    title: "automatic breaks",
+    isActive: true,
+  },
+  {
+    title: "automatic sessions",
+    isActive: true,
+  },
+  {
+    title: "show notification",
+    isActive: true,
+  },
+]
