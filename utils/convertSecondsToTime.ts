892f4851b881b0c3c82e6de9547669871cadb2d4
João Pedro <joaopedro.nc@outlook.com>
2023-10-19 16:09:48 -0300

    ✨ feat (utils): add convertSecondsToTime util function

--- utils/convertSecondsToTime.ts
+++ utils/convertSecondsToTime.ts
@@ -1,5 +1,12 @@
-export function convertSecondsToTime(seconds: number) {
+import { formatTime } from "./formatTime"
+
+export function convertSecondsToTime(seconds: number, canJoin = false) {
   const minutesValue = Math.floor(seconds / 60)
+  const secondsValue = seconds % 60
+
+  if (canJoin) {
+    return `${formatTime(minutesValue)}:${formatTime(secondsValue)}`
+  }
 
   if (minutesValue > 1) {
     return `${minutesValue} minutes`
