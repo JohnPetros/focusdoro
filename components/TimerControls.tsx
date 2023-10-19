f558f62ad4e2625920eb094f8d1607611c5cfc8d
João Pedro <joaopedro.nc@outlook.com>
2023-10-19 20:06:12 -0300

    ✨ feat (ui): add TimerControls component

---
+++ components/TimerControls.tsx
@@ -0,0 +1,79 @@
+import {
+  ClockCounterClockwise,
+  Pause,
+  Play,
+  Square as Reset,
+} from "phosphor-react-native"
+import { XStack, YStack } from "tamagui"
+
+import { useTimerStore } from "../hooks/useTimerStore"
+
+import { TimerControlButton } from "./TimerControlButton"
+
+interface TimerControlsProps {
+  isTimerLoaded: boolean
+}
+
+export function TimerControls({ isTimerLoaded }: TimerControlsProps) {
+  const {
+    state: { isPaused, sessionSeconds },
+    action: {
+      setIsPaused,
+      setIsBreak,
+      setIsLongBreak,
+      setSessionSeconds,
+      setCompletedSessions,
+      setShouldReset,
+    },
+  } = useTimerStore()
+
+  function handlePlayButton() {
+    if (isTimerLoaded) setIsPaused(!isPaused)
+  }
+
+  function handleResetSessionButton() {
+    setShouldReset(true)
+    setIsPaused(false)
+  }
+
+  function handleResetPomodoroButton() {
+    setIsPaused(false)
+    setIsBreak(false)
+    setIsLongBreak(false)
+
+    setSessionSeconds(sessionSeconds)
+    setCompletedSessions(1)
+  }
+
+  return (
+    <YStack
+      position="absolute"
+      ai="center"
+      gap={16}
+      bottom={10}
+    >
+      <TimerControlButton
+        icon={isPaused ? Play : Pause}
+        label={`${isPaused ? "Play" : "Pause"} timer"`}
+        onPress={handlePlayButton}
+        isLarge
+      />
+
+      {isPaused && (
+        <XStack gap={64}>
+          <TimerControlButton
+            icon={ClockCounterClockwise}
+            label="Reset current session"
+            onPress={handleResetSessionButton}
+          />
+
+          <TimerControlButton
+            icon={Reset}
+            label="Reset pomodoro"
+            onPress={handleResetPomodoroButton}
+          />
+        </XStack>
+      )}
+    </YStack>
+  )
+}
