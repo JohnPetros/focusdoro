bf4727fac86bc2ced7ccb8ae1c5c82ec88305eb8
João Pedro <joaopedro.nc@outlook.com>
2023-10-19 16:10:13 -0300

    🐛 fix: ITimerNotification interface

--- services/notification/interfaces/ITimerNotification.ts
+++ services/notification/interfaces/ITimerNotification.ts
@@ -1,12 +1,21 @@
+import { Event } from "@notifee/react-native"
+
 import { TimerNotificationAction } from "../../../@types/timer-notification-action"
 
 interface UpdateTimerParams {
   title: string
   progress: number
   actions: TimerNotificationAction[]
+  time: string
 }
 export interface ITimerNotification {
   displayTimer(taskId: string): Promise<void>
-  updateTimer({ title, actions }: UpdateTimerParams): Promise<void>
+  updateTimer({
+    title,
+    actions,
+    progress,
+    time,
+  }: UpdateTimerParams): Promise<void>
   cancelTimer(): Promise<void>
+  onTimerAction(callback: (notificationEvent: Event) => void): void
 }
