5b855f9147f1cd67479ca77237329ad31e197e81
João Pedro <joaopedro.nc@outlook.com>
2023-10-19 21:24:20 -0300

    💄 style: decrease TaskControlButton opacity when disabled

--- components/TaskControlButton.tsx
+++ components/TaskControlButton.tsx
@@ -1,5 +1,5 @@
 import { Icon } from "phosphor-react-native"
-import { useTheme } from "tamagui"
+import { useTheme, View } from "tamagui"
 
 import { RoundButton } from "./RoundButton"
 
@@ -7,30 +7,35 @@ interface TaskControlButtonProps {
   icon: Icon
   label: string
   onPress: () => void
+  isDisabled?: boolean
 }
 
 export function TaskControlButton({
   icon: Icon,
   label,
+  isDisabled,
   onPress,
 }: TaskControlButtonProps) {
   const theme = useTheme()
 
   return (
-    <RoundButton
-      shadowColor={theme.blue12.val}
-      size="$2"
-      radius={18}
-      icon={
-        <Icon
-          color={theme.blue12.val}
-          size={16}
-          weight="bold"
-        />
-      }
-      bc="$blue2"
-      onPress={onPress}
-      aria-label={label}
-    />
+    <View opacity={isDisabled ? 0.5 : 1}>
+      <RoundButton
+        shadowColor={theme.blue12.val}
+        size="$2"
+        radius={18}
+        icon={
+          <Icon
+            color={theme.blue12.val}
+            size={16}
+            weight="bold"
+          />
+        }
+        bc="$blue2"
+        onPress={onPress}
+        aria-label={label}
+        disabled={isDisabled}
+      />
+    </View>
   )
 }
