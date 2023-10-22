fc121fa2d50400775fb180e835de1ae6d2a67277
João Pedro <joaopedro.nc@outlook.com>
2023-10-22 20:07:54 -0300

    🏷️ type: add AudioTitle type

---
+++ @types/audio.ts
@@ -0,0 +1,8 @@
+import { Icon } from "phosphor-react-native"
+
+export type AudioTitle = "forest" | "cafe" | "ocean" | "rain" | "peace"
+
+export type Audio = {
+  title: AudioTitle
+  icon: Icon
+}
