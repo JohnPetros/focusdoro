5896dcc2693e2edd45ecfb2d78a2c8c0e3521088
João Pedro <joaopedro.nc@outlook.com>
2023-10-22 16:58:39 -0300

    🧪 test: AudioModal component

--- __tests__/components/AudioModal.test.tsx
+++ __tests__/components/AudioModal.test.tsx
@@ -1,27 +1,96 @@
-import { render, screen } from "@testing-library/react-native"
+import { act } from "react-test-renderer"
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react-native"
 
+import type { AudioTitle } from "../../@types/audio"
 import { AudioModal, AudioModalContent } from "../../components/AudioModal"
-import { BackgroundAudioProvider } from "../../contexts/BackgroundAudioContext"
+import { BackgroundAudioContext } from "../../contexts/BackgroundAudioContext"
 import { TamaguiProvider } from "../../providers/TamaguiProvider"
 import { AUDIOS } from "../../utils/audios"
 
 const setIsModalOpenMock = jest.fn()
+const loadAudioMock = jest.fn()
+const playMock = jest.fn()
+const stopMock = jest.fn()
+const storeAudioMock = jest.fn()
 
-function renderCompoenent() {
+async function renderComponent() {
   render(
     <TamaguiProvider>
-      <BackgroundAudioProvider>
+      <BackgroundAudioContext.Provider
+        value={{
+          audio: "cafe",
+          isLoaded: true,
+          loadAudio: loadAudioMock,
+          play: playMock,
+          stop: stopMock,
+          storeAudio: storeAudioMock,
+        }}
+      >
         <AudioModal open={true}>
           <AudioModalContent setIsModalOpen={setIsModalOpenMock} />
         </AudioModal>
-      </BackgroundAudioProvider>
+      </BackgroundAudioContext.Provider>
     </TamaguiProvider>
   )
 }
 
 describe("AudioModal component", () => {
-  it.each(AUDIOS)("should render $title audio", ({ title }) => {
-    renderCompoenent()
-    expect(screen.getByTestId(title)).toBeOnTheScreen()
+  it.each(AUDIOS)("should render $title audio", async ({ title }) => {
+    renderComponent()
+
+    await waitFor(() => {
+      expect(screen.findByText(title)).toBeTruthy()
+    })
+  })
+
+  it("should close when close button is pressed", async () => {
+    renderComponent()
+
+    await act(() => {
+      fireEvent.press(screen.getByLabelText("Close modal"))
+    })
+
+    await waitFor(() => {
+      expect(setIsModalOpenMock).toHaveBeenCalledWith(false)
+    })
+  })
+
+  it("should close when confirm button is pressed", async () => {
+    renderComponent()
+
+    fireEvent.press(screen.getByText("play audio"))
+
+    await waitFor(() => {
+      expect(setIsModalOpenMock).toHaveBeenCalledWith(false)
+    })
+  })
+
+  it("should close when confirm button is pressed", async () => {
+    renderComponent()
+
+    fireEvent.press(screen.getByText("play audio"))
+
+    await waitFor(() => {
+      expect(setIsModalOpenMock).toHaveBeenCalledWith(false)
+    })
+  })
+
+  it("should store audio when a checkbox is pressed", async () => {
+    renderComponent()
+
+    const audio: AudioTitle = "cafe"
+
+    const checkbox = screen.getByText(audio)
+
+    fireEvent.press(checkbox)
+
+    await waitFor(() => {
+      expect(storeAudioMock).toHaveBeenCalledWith(audio)
+    })
   })
 })
