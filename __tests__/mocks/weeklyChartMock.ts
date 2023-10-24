3a9f2cf5aeb55202354c44216a3bdd0ba4044915
João Pedro <joaopedro.nc@outlook.com>
2023-10-24 12:27:07 -0300

    🧪 test: add weeklyChartMock

---
+++ __tests__/mocks/weeklyChartMock.ts
@@ -0,0 +1,35 @@
+import { WeeklyChart } from "../../@types/weeklyChart"
+
+export const weeklyChartMock: WeeklyChart = {
+  shouldReset: false,
+  weekdays: [
+    {
+      weekday: "Sun",
+      value: 5,
+    },
+    {
+      weekday: "Mon",
+      value: 2,
+    },
+    {
+      weekday: "Tue",
+      value: 1,
+    },
+    {
+      weekday: "Wed",
+      value: 0,
+    },
+    {
+      weekday: "Thu",
+      value: 4,
+    },
+    {
+      weekday: "Fri",
+      value: 4,
+    },
+    {
+      weekday: "Sat",
+      value: 8,
+    },
+  ],
+}
